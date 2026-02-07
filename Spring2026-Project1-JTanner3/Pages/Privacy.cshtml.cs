4925ec102a266619ec418464684e4159450705c0
JTanner04 <jtanner3@crimson.ua.edu>
2026-02-07 13:53:16 -0600

    Add project files.

---
+++ Spring2026-Project1-JTanner3/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spring2026_Project1_JTanner3.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
